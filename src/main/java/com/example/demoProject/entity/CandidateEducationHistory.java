0150c09b263493386dadd119459ab74de8eec9e4
sameer tripathi <sameertrip123@gmail.com>
2024-04-11 18:45:13 +0530

    Add fields in CandidateEducationHistory entity

--- src/main/java/com/example/demoProject/entity/CandidateEducationHistory.java
+++ src/main/java/com/example/demoProject/entity/CandidateEducationHistory.java
@@ -1,5 +1,34 @@
 package com.example.demoProject.entity;
 
-public class CandidateEducationHistory {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
+@Entity
+@Table(name = "candidate_education_table")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CandidateEducationHistory {
+    private int id;
+
+    private String instituteName;
+
+    private String educationQualification;
+
+    private String educationSpecialization;
+
+    private String educationLocation;
+
+    private Date educationStartDate;
+
+    private Date educationEndDate;
+
+    private String educationDescription;
+
+    private boolean isManuallyAdded;
 }
--- src/main/java/com/example/demoProject/service/CandidateService.java
+++ src/main/java/com/example/demoProject/service/CandidateService.java
@@ -31,5 +31,4 @@ public class CandidateService {
         }
         return candidate.get();
     }
-
 }
